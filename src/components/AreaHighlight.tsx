bfb114a3db3c04b70e66444ce57c65d8b9873fc7
DanielArnould <darn0005@student.monash.edu>
2023-11-11 20:52:18 +1100

    Made AreaHighlight functional
    Made the AreaHighlight component an arrow functional component with documentation.

--- src/components/AreaHighlight.tsx
+++ src/components/AreaHighlight.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { CSSProperties } from "react";
 
 import { Rnd } from "react-rnd";
 import { getPageFromElement } from "../lib/pdfjs-dom";
@@ -8,60 +8,71 @@ import "../style/AreaHighlight.css";
 import type { LTWHP, ViewportHighlight } from "../types";
 
 interface Props {
+  /** The highlight to associate with the area. */
   highlight: ViewportHighlight;
+
+  /**
+   * A callback function called when the highlight area changes.
+   * @param {LTWHP} rect - The updated highlight area.
+   */
   onChange: (rect: LTWHP) => void;
+
+  /** Indicates whether the component is scrolled into view. */
   isScrolledTo: boolean;
+
+  /** Additional CSS styles for the component. */
+  style?: CSSProperties;
 }
 
-export class AreaHighlight extends Component<Props> {
-  render() {
-    const { highlight, onChange, isScrolledTo, ...otherProps } = this.props;
+/**
+ * A component for displaying and interacting with a rectangular highlight area.
+ *
+ * @param {Props} props - The component's properties.
+ */
+const AreaHighlight = ({ highlight, onChange, isScrolledTo, style }: Props) => {
+  const highlightClass = isScrolledTo ? "AreaHighlight --scrolledTo" : "";
 
-    return (
-      <div
-        className={`AreaHighlight ${
-          isScrolledTo ? "AreaHighlight--scrolledTo" : ""
-        }`}
-      >
-        <Rnd
-          className="AreaHighlight__part"
-          onDragStop={(_, data) => {
-            const boundingRect: LTWHP = {
-              ...highlight.position.boundingRect,
-              top: data.y,
-              left: data.x,
-            };
+  return (
+    <div className={`AreaHighlight ${highlightClass}`}>
+      <Rnd
+        className="AreaHighlight__part"
+        onDragStop={(_, data) => {
+          const boundingRect: LTWHP = {
+            ...highlight.position.boundingRect,
+            top: data.y,
+            left: data.x,
+          };
 
-            onChange(boundingRect);
-          }}
-          onResizeStop={(_mouseEvent, _direction, ref, _delta, position) => {
-            const boundingRect: LTWHP = {
-              top: position.y,
-              left: position.x,
-              width: ref.offsetWidth,
-              height: ref.offsetHeight,
-              pageNumber: getPageFromElement(ref)?.number || -1,
-            };
+          onChange(boundingRect);
+        }}
+        onResizeStop={(_mouseEvent, _direction, ref, _delta, position) => {
+          const boundingRect: LTWHP = {
+            top: position.y,
+            left: position.x,
+            width: ref.offsetWidth,
+            height: ref.offsetHeight,
+            pageNumber: getPageFromElement(ref)?.number || -1,
+          };
 
-            onChange(boundingRect);
-          }}
-          position={{
-            x: highlight.position.boundingRect.left,
-            y: highlight.position.boundingRect.top,
-          }}
-          size={{
-            width: highlight.position.boundingRect.width,
-            height: highlight.position.boundingRect.height,
-          }}
-          onClick={(event: Event) => {
-            event.stopPropagation();
-            event.preventDefault();
-          }}
-          {...otherProps}
-        />
-      </div>
-    );
-  }
-}
+          onChange(boundingRect);
+        }}
+        position={{
+          x: highlight.position.boundingRect.left,
+          y: highlight.position.boundingRect.top,
+        }}
+        size={{
+          width: highlight.position.boundingRect.width,
+          height: highlight.position.boundingRect.height,
+        }}
+        // Prevevent any event clicks as clicking is already used for movement
+        onClick={(event: Event) => {
+          event.stopPropagation();
+          event.preventDefault();
+        }}
+        style={style}
+      />
+    </div>
+  );
+};
 
 export default AreaHighlight;
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export { PdfHighlighter } from "./components/PdfHighlighter";
 export { Tip } from "./components/Tip";
 export { Highlight } from "./components/Highlight";
 import Popup from "./components/Popup";
-export { AreaHighlight } from "./components/AreaHighlight";
+import AreaHighlight from "./components/AreaHighlight";
 export { PdfLoader } from "./components/PdfLoader";
 
-export { Popup };
+export { Popup, AreaHighlight };
 export * from "./types";
